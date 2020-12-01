1a8a59be39c633b2f56e47a3857dcd99e6634e1a
Klupea <insatcoma@outlook.fr>
2020-12-01 18:46:22 +0100

    minimalist project and gitignore

---
+++ src/main/java/App.java
@@ -0,0 +1,35 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.logging.Logger;
+
+public class App {
+
+    private static Logger log = Logger.getLogger(App.class.getName());
+
+
+    public static void main(String[] args){
+
+        LocalDate nextFriday13 = LocalDate.now();
+
+        log.info("TODAY IS : "+nextFriday13.toString());
+        log.info("DAY of Month today is : "+nextFriday13.getDayOfMonth());
+
+
+        //find next friday (any friday)
+        while(DayOfWeek.FRIDAY != nextFriday13.getDayOfWeek()) {
+            nextFriday13 = nextFriday13.plusDays(1);
+        }
+
+        log.info("next ordinary friday will be "+nextFriday13.toString());
+
+        //here we are sure that the var contains a friday
+        while(13 != nextFriday13.getDayOfMonth()){
+            nextFriday13 = nextFriday13.plusDays(7);
+        }
+
+
+        log.info("next friday 13 is : "+nextFriday13.toString()
+                +"\n"+"will be in "+ ChronoUnit.DAYS.between(LocalDate.now(), nextFriday13)+" days");
+    }
+}
